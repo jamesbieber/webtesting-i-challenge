342d350c7e86b09fa8caaf8b4f851b0ada09dd87
jamesbieber <jamesbieber97@gmail.com>
2019-06-19 20:09:28 -0700

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,16 +6,46 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (!item || !item.enhancement) {
+    return "Enhancement missing or no item passed.";
+  }
+
+  if (item.enhancement === 20) {
+    return { ...item };
+  } else {
+    item.enhancement += 1;
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const test = item;
+
+  if (!test || !test.enhancement) {
+    return "Enhancement missing or no item passed.";
+  }
+
+  if (test.enhancement < 15) {
+    test.durability -= 5;
+    return test;
+  } else {
+    test.durability -= 10;
+    if (test.enhancement > 16) {
+      test.enhancement -= 1;
+      return test;
+    }
+    return test;
+  }
 }
 
 function repair(item) {
-  console.log(item);
-  return item.durability;
+  const test = item;
+  if (!test || !test.durability) {
+    return "Durability missing or no item passed.";
+  } else {
+    test.durability = 100;
+    return test;
+  }
 }
 
 function get(item) {
