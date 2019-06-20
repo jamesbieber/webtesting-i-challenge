f401feb242e7632df9f991c90fc79a708bb205a7
jamesbieber <jamesbieber97@gmail.com>
2019-06-19 19:07:42 -0700

    Start of project

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,7 +2,7 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
@@ -14,7 +14,8 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  console.log(item);
+  return item.durability;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { repair, succeed, fail, get } = require("./enhancer.js");
+
+const item = {
+  name: "Sword of Jest",
+  durability: 30,
+  enchantment: 15
+};
+
+describe("the repair function", () => {
+  it("should return an error message if item or durability doesnt exist", () => {
+    expect(repair().toBe("No item was passed"));
+  });
+
+  it("should set the durability of an item to 100", () => {
+    expect(repair(item).toBe(100));
+  });
+});
